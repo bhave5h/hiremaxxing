ab3deddc67f1fb4e0d7a97261620c465156889ee
Bhavesh Chawre <bhaveshchawre1@gmail.com>
2026-09-05 17:46:08 +0530

    sign in

--- proxy.ts
+++ proxy.ts
@@ -1,11 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
-  "/",
   "/sign-in(.*)",
   "/sign-up(.*)",
-  "/talent(.*)",
-  "/gigs(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, request) => {
